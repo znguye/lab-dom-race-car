8263667c5e1050a423341a07799d9b29e30eb888
ross-u <cirkovic.uros.srb@gmail.com>
2023-04-06 10:53:12 +0200

    feat: add starter code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+
+  function startGame() {
+    console.log("start game");
+  }
+};
